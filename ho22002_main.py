fcd239b51ca809094ba8f10fa2f5bf2ba81f8ee1
Henrijs Obolevičs <bonis.obo@gmail.com>
2023-11-30 17:46:48 +0200

    Initial commit

---
+++ ho22002_main.py
@@ -0,0 +1,8 @@
+# This is a simple calculation program
+# created to demonstrate version control.
+from ho22002_libadd import *
+from ho22002_libmult import *
+a = 13
+b = 5
+print(plus(a,b))
+print(mult(a,b))
